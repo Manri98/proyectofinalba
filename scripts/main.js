d554d7f7af6a499d5955b3d0376bc5badd6c6f77
Nicolas <manrique.nicolas28@gmail.com>
2023-06-14 22:54:01 -0300

    proyectofinal

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+    document.getElementById("viewAlerta").addEventListener("click", function () {
+        alert("Enviado! Recibiras informacion a la brevedad. Gracias por contactarnos.");
+
+    })
+
+
+})
